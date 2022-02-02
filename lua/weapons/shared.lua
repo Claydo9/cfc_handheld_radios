66614b119227d0321514d9158380d5139161de23
Claydo <claydo9@gmail.com>
2022-02-02 22:33:46 +1000

    Initial SWEP Setup

---
+++ lua/weapons/shared.lua
@@ -0,0 +1,15 @@
+SWEP.Author             = "Claydo9"
+SWEP.Contact            = "CFC Servers"
+SWEP.Instructions       = "Left click to toggle radio on/off, Right click to display tuninng menu"
+
+SWEP.Spawnable          = true 
+
+SWEP.ViewModel          = ""
+SWEP.WorldModel         = ""
+
+SWEP.Primary.ClipSize    = 1
+SWEP.Primary.Delay       = 0.5
+SWEP.Primary.DefaultClip = 1
+SWEP.Primary.Automatic   = false
+SWEP.Primary.Ammo        = "none"
+
