cf154716b4812c7707a4d6344a63cb9e65452db8
Claydo <claydo9@gmail.com>
2022-02-03 02:02:13 +1000

    Compact code into fewer files

--- lua/weapons/cfc_handheld_radio/shared.lua
+++ lua/weapons/cfc_handheld_radio/shared.lua
@@ -1,11 +1,11 @@
-SWEP.Author             = "Claydo9"
-SWEP.Contact            = "CFC Servers"
-SWEP.Instructions       = "Left click to toggle radio on/off, Right click to display tuninng menu"
+SWEP.Author              = "Claydo9"
+SWEP.Contact             = "CFC Servers"
+SWEP.Instructions        = "Left click to toggle radio on/off, Right click to display tuning menu"
 
-SWEP.Spawnable          = true 
+SWEP.Spawnable           = true 
 
-SWEP.ViewModel          = ""
-SWEP.WorldModel         = ""
+SWEP.ViewModel           = ""
+SWEP.WorldModel          = ""
 
 SWEP.Primary.ClipSize    = 1
 SWEP.Primary.Delay       = 0.5
@@ -13,3 +13,55 @@ SWEP.Primary.DefaultClip = 1
 SWEP.Primary.Automatic   = false
 SWEP.Primary.Ammo        = "none"
 
+handheldActive = false
+handheldVguiExists = false
+
+if CLIENT then
+    SWEP.PrintName          = "Walkie Talkie" 
+    SWEP.Category           = "CFC"
+
+    SWEP.Slot               = 1
+    SWEP.SlotPos            = 1
+
+    SWEP.DrawCrosshair      = false
+    SWEP.DrawAmmo           = false
+
+    function SWEP:PrimaryAttack()
+        handheldActive = not handheldActive
+        print( handheldActive )
+    end
+
+    function SWEP:SecondaryAttack()
+        if not handheldActive or handheldVguiExists then return end
+
+        local resX, resY    = ScrW(), ScrH()
+        local scale         = 0.7
+        local padding       = 10
+
+        local tunerBase     = vgui.Create( "DFrame" )
+        tunerBase:SetSize( ( resX / scale ) - padding, ( resY / scale ) - padding )
+        tunerBase:SetPos( ResX - ( tunerBase:GetWide( ) / 2 ) - padding, ResY - ( tunerBase:GetWide( ) / 2 ) - padding )
+        tunerBase:SetVisible( true ) 
+        tunerBase:SetDraggable( true )
+        tunerBase:MakePopup( )
+        
+    end
+
+    function SWEP:TakePrimaryAmmo()
+        return 
+    end 
+end
+
+if SERVER then
+    function SWEP:PrimaryAttack() 
+        return
+    end 
+
+    function SWEP:SecondaryAttack() 
+        return
+    end 
+
+    function SWEP:TakePrimaryAmmo()
+        return
+    end
+end
